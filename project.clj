b13c09249b7cffbaa5da7b4f215c247ab5bf0ab1
Erik Nomitch <enomitch@gmail.com>
2014-08-10 13:11:14 -0500

    initial commit; working overtone + monome_serial + kick

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject example "1.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.9.1"]
+                 [org.clojure/data.json "0.2.3"]
+                 [clj-native "0.9.3"]
+                 [overtone/at-at "1.2.0"]
+                 [overtone/osc-clj "0.9.0"]
+                 [overtone/byte-spec "0.3.1"]
+                 [overtone/midi-clj "0.5.0"]
+                 [overtone/libs.handlers "0.2.0"]
+                 [overtone/scsynth "3.5.7.0"]
+                 [overtone/scsynth-extras "3.5.7.0"]
+                 [clj-glob "1.0.0"]
+                 [monome-serial "0.3.1"]
+                 ])
---
+++ src/example/core.clj
@@ -0,0 +1,25 @@
+(ns example.core
+  (:use [overtone.live])
+  (:use [overtone.sc.machinery.synthdef])
+  (:use [clojure.pprint]))
+
+(use 'overtone.live)
+
+(defsynth kick-drum [amp 1 decay 0.8 freq 45 attack 10]
+  (let [env (env-gen (perc 0 decay) 1 1 0 1 FREE)
+        snd (sin-osc freq (* Math/PI 0.5))]
+    (out 0 (pan2 (* snd env) 0))))
+
+(def one-twenty-bpm (metronome 120))
+
+(defn looper [nome sound]
+    (let [beat (nome)]
+        (at (nome beat) (sound))
+        (apply-by (nome (inc beat)) looper nome sound [])))
+
+;; LOOPERS
+(looper one-twenty-bpm kick-drum)
+;(looper one-twenty-bpm snare-drum)
+
+(stop)
+
